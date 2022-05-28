7cf54307943be0a6addb2535e8cf1247eed56f4a
GuDimz <zgudimz@gmail.com>
2022-05-28 13:50:18 +0300

    268

---
+++ Easy/268_MissingNumber/MissingNumber.java
@@ -0,0 +1,10 @@
+
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum = nums.length * (nums.length + 1) / 2;
+        for (int i : nums)
+            sum -= i;
+        return sum;
+    }
+}
